049272fa7211d397ed3e88b863a7698bd84c87f8
Vignesh Bhat N <vigneshbhatn27@gmail.com>
2025-05-04 14:29:43 +0530

    created earning model

--- app/models/earning_model.py
+++ app/models/earning_model.py
@@ -2,3 +2,13 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+
+Base= declarative_base()
+
+class Earning(Base):
+    __tablename__ = 'earnings'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)  # Unique identifier for each expense.
+    amount = Column(Float, nullable=False)
+    source = Column(String(100), nullable=False)
+    date = Column(DateTime, nullable=False)
--- app/models/expense_model.py
+++ app/models/expense_model.py
@@ -13,4 +13,4 @@ class Expense(Base):
     amount = Column(Float, nullable=False)  # Amount spent (must be a number).
     category = Column(String(100), nullable=False)  # Category of the expense (e.g., Food, Transportation).
     description = Column(String(255), nullable=True)  # Optional description.
-    date = Column(DateTime, default=datetime.utcnow, nullable=False)  # Date of expense (defaults to current time).
+    date = Column(DateTime, default=datetime, nullable=False)  # Date of expense (defaults to current time).

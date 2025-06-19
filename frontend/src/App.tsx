4a8758239a780c4233bc27ffdb27f12f45ce4ef6
Vignesh Bhat N <vigneshbhatn27@gmail.com>
2025-06-19 19:38:40 +0530

    reorganised directory

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-400">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
